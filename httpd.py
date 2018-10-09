76c2b6deac7dd5628967ba74b990d57cfdd99f37
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-10-09 17:34:30 +0400

    + Sym class tree + FVM
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- httpd.py
+++ httpd.py
@@ -1,4 +1,53 @@
 #!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+## @file
+## @brief httpd server using Flask
+
+## @brief core generic object
+class Qbject:
+    def __init__(self,V):
+        self.type  = self.__class__.__name__.lower()
+        self.value = V
+    def __repr__(self):
+        return self.dump()
+    def dump(self,depth=0):
+        return self.head()
+    def head(self,prefix=''):
+        return '%s<%s:%s>'%(prefix,self.type,self.value)
+
+## @brief primitive
+class Primitive(Qbject): pass
+
+## @brief symbol names sw and model entities 
+class Symbol(Primitive): pass
+
+## @brief number
+class Number(Primitive): pass
+
+## @brief string
+class String(Primitive): pass
+
+## @brief data container
+class Container(Qbject): pass
+
+## @brief LIFO stack
+class Stack(Container): pass
+
+## @brief associative (key/value) array 
+class Map(Container): pass
+
+## @brief client-side gramma for PEG.js
+PEGJS = String('''// PEG.js ''')
+
+## @brief client-side command field template
+PAD = String('\ FORTH command')
+
+## @brief ForthVM stack
+S = Stack('DATA')
+
+## @brief ForthVM vocabulary
+W = Map('FORTH')
 
 import flask
 
@@ -6,6 +55,6 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return flask.render_template('index.html')
+    return flask.render_template('index.html',S=S,W=W,PEGJS=PEGJS,PAD=PAD)
 
 app.run(debug=True,host='0.0.0.0',port=8888)
