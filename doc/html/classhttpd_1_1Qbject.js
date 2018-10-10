80e4f18921c2daf9941175ff687b228d4a5a11ba
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-10-10 08:55:23 +0400

    * html manual via github http
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- doc/html/classhttpd_1_1Qbject.js
+++
@@ -1,9 +0,0 @@
-var classhttpd_1_1Qbject =
-[
-    [ "__init__", "classhttpd_1_1Qbject.html#a2f2080c78bc48ab0e12d1b52f87dbe9a", null ],
-    [ "__repr__", "classhttpd_1_1Qbject.html#a37f3d1204f3a3eff9a142f453a4c4190", null ],
-    [ "dump", "classhttpd_1_1Qbject.html#ae8295f08ab5e5f177116f96e3895ea11", null ],
-    [ "head", "classhttpd_1_1Qbject.html#a63dde6700a336e12d24083b5b7f250b9", null ],
-    [ "type", "classhttpd_1_1Qbject.html#af22970305a34408e250146e6641900ec", null ],
-    [ "value", "classhttpd_1_1Qbject.html#a3f92ccf3e9c781975b9d1fe7ba24820a", null ]
-];
