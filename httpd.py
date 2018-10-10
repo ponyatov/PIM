a321ca1b58b82a2ebdd777f26ba7b7aa4cf2fe34
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-10-10 11:23:20 +0400

    + login form
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- httpd.py
+++ httpd.py
@@ -6,6 +6,7 @@
 import os,sys
 
 from SYM import *
+from attr import validators
 
 ## @brief client-side gramma for PEG.js
 PEGJS = String('''// PEG.js ''')
@@ -20,13 +21,30 @@ S = Stack('DATA')
 W = Map('FORTH')
 
 import flask
-import flask_wtf
+import flask_wtf,wtforms
 
 app = flask.Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY') or 'you-will-never-guess'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY') \
+                            or 'you-will-never-guess'
 
 @app.route('/')
 def index():
     return flask.render_template('index.html',S=S,W=W,PEGJS=PEGJS,PAD=PAD)
+
+@app.route('/docs/<path:path>')
+def docs(path):
+    return flask.send_from_directory('docs',path)
+
+class LoginForm(flask_wtf.FlaskForm):
+    login  = wtforms.StringField('user',\
+                validators = [wtforms.validators.DataRequired()])
+    passwd = wtforms.PasswordField('password',\
+                validators = [wtforms.validators.DataRequired()])
+    cookie = wtforms.BooleanField('remember me')
+    submit = wtforms.SubmitField('login')
+    
+@app.route('/login')
+def login():
+    return flask.render_template('login.html',form=LoginForm())
 
 app.run(debug=True,host='0.0.0.0',port=8888)
